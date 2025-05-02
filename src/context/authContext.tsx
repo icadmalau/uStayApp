a9388262ee36b398f86e34489d477069c4c17d0b
Projectdatascience <projectdatascience864@example.com>
2025-05-02 23:42:09 +0700

    update

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -4,47 +4,45 @@ interface AuthContextProps {
     user: any;
     login: (userData: any) => void;
     logout: () => void;
-  }
+}
 
 const AuthContext = createContext<AuthContextProps | null>(null);
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
 };
 
 interface AuthProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
+    const [user, setUser ] = useState<any>(null);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-    setLoading(false);
-  }, []);
+    useEffect(() => {
+        const storedUser  = localStorage.getItem('user');
+        if (storedUser ) {
+            setUser (JSON.parse(storedUser ));
+        }
+    }, []);
 
-  const login = (userData: any) => {
-    setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
-  };
+    const login = (userData: any) => {
+        setUser (userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+    };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-  };
+    const logout = () => {
+        setUser (null);
+        localStorage.removeItem('user');
+    };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
