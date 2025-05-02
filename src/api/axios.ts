5a228c9ab82685136f340fcf80e2ea5be8272812
Projectdatascience <projectdatascience864@example.com>
2025-05-03 00:51:13 +0700

    set url backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // URL backend
+  baseURL: 'https://ustaybackend-production.up.railway.app/', // URL backend
   headers: {
     'Content-Type': 'application/json',
    
